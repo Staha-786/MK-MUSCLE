9489c0aac7e05f37ddcb1bc54b72d6ff1b593f0f
Taha <syedtahah28@gmail.com>
2026-08-09 01:53:56 +0500

    Fix WhatsApp links

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,7 +1,7 @@
 export const WHATSAPP_NUMBER = '923351225538';
 
 export const WHATSAPP_MESSAGE =
-  'Hi, I am interested in MK Muscle Gym';
+  'Hi, I want to join MK Muscle Gym, please share membership details';
 
 export const WHATSAPP_LINK =
   `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(WHATSAPP_MESSAGE)}`;
